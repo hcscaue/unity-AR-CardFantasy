d8183021bf5fc0f7344a01f95e9acaa0b780477d
Cauehcs <caue.hcs@gmail.com>
2020-12-03 11:30:43 -0300

    Projeto iniciado e Criação dos Scriptableobjects(script)

---
+++ Assets/Scripts/CardEffect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card/Effect")]
+public class CardEffect : ScriptableObject {
+
+    public string cardName;
+
+    [Multiline]
+    public string description;
+
+    public int mana;
+
+    public Animator animator;                               
+
+}
---
+++ Assets/Scripts/CardHero.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card/Hero")]
+public class CardHero : ScriptableObject
+{
+
+    public string heroName;
+
+    [Multiline]
+    public string descriptionHero;
+
+    public int Life, Attack;
+    public float multiplyAttack;
+
+    public Animator animator;
+
+    public enum Special { Shielded, Commandment, Enraged }
+    public Special special;
+
+    [Multiline]
+    public string descriptionSpecial;
+    public int cooldown;
+
+}
